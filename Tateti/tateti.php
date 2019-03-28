138c47ca618b91dccd88e1fcd41240493ac2bdd2
MarconiMauro <48569234+MarconiMauro@users.noreply.github.com>
2019-03-28 14:42:35 -0300

    Create tateti.php

---
+++ Tateti/tateti.php
@@ -0,0 +1,31 @@
+<?php
+
+Class Tateti
+{
+    public function TableTateti()
+    {
+        $ar = array();
+        $ar [] = array(1,2,3);
+        $ar [] = array(4,5,6);
+        $ar [] = array(7,8,9);
+    }
+    while ($win = false && (!empty ($ar)))
+
+    public function StatusTateti()
+    {
+        $status = true;
+        if (! empty($ar) )
+        {
+            if($ar = Null)
+            {
+                if($status = false)
+                {  
+                    return $ar[$status];
+                }
+            return $ar [$status];
+            }
+            echo 'Ya hay un valor en esa posicion';
+        }
+    }
+}
+

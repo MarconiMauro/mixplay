560583bb1406b7e338e52fe0c1e4f0e5e7613746
MarconiMauro <48569234+MarconiMauro@users.noreply.github.com>
2019-04-05 13:18:34 -0300

    Create Bici.php

---
+++ caja/Bici.php
@@ -0,0 +1,33 @@
+<?php
+
+class Bici implements InterfaceObjeto
+{
+    
+    private $bicis = [];
+    /*
+    public function addFile(ComponentInterface $file)
+    {
+        $this->files[$file->id] = $file;
+ 
+        return $this;
+    }
+    */
+
+    public function nameVolumen($vol)
+    {
+
+            $this->bici[$bici->vol] = $vol;
+            return $this;
+        
+    }
+
+    public function nombre($name)
+    {
+            return $this->bici;
+    }
+    
+    public function mostrar()
+    {
+
+    }
+}

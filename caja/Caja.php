efe4b42c33565d1628aef66116351e6a796c55b9
MarconiMauro <48569234+MarconiMauro@users.noreply.github.com>
2019-04-05 13:19:44 -0300

    Create Caja.php

---
+++ caja/Caja.php
@@ -0,0 +1,23 @@
+<?php
+
+Class Caja
+{
+    //volumen int
+    //contenido array[objeto]
+    $ar = array();
+
+    function guardar()
+    {
+
+    }
+
+    function vaciar()
+    {
+
+    }
+
+    function sacarObjeto()
+    {
+        
+    }
+}

6e0591c16b4a421f22e8ad113562e16fbf3862d5
MarconiMauro <48569234+MarconiMauro@users.noreply.github.com>
2019-04-05 13:18:58 -0300

    Create Taza.php

---
+++ caja/Taza.php
@@ -0,0 +1,19 @@
+<?php
+
+class Taza implements InterfaceObject
+{
+    private $taza;
+
+    public function setTaza()
+    {      
+            $this->taza = $taza;
+            return $this;
+        
+    }
+
+    public function getTaza()
+    {
+            return $this->taza;
+    }
+    
+}
